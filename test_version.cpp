aed68d8e3121e06ffb8ee4d5c327a37edfb7e762
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-01-14 20:16:52 +0300

    Update test_version.cpp

--- test_version.cpp
+++ test_version.cpp
@@ -1,14 +1,19 @@
-#define BOOST_TEST_MODULE test_version
+//#define BOOST_TEST_MODULE test_version
 
+#include <gtest/gtest.h>
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
-{
-    BOOST_CHECK(version() > 0);
+TEST(VersionTest, VersionIsGreaterThan0) {
+   ASSERT_GT(version(), 0)<<"version is greater than 0";
 }
 
-}
+//#include <boost/test/unit_test.hpp>
+
+//BOOST_AUTO_TEST_SUITE(test_version)
+
+//BOOST_AUTO_TEST_CASE(test_valid_version)
+//{
+//    BOOST_CHECK(version() > 0);
+//}
+
+//}
