cc7a37e870b5ddf6f738291f6b7fd90b41620236
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-01-14 20:30:48 +0300

    Update test_version.cpp

--- test_version.cpp
+++ test_version.cpp
@@ -6,7 +6,10 @@
 TEST(VersionTest, VersionIsGreaterThan0) {
    ASSERT_GT(version(), 0)<<"version is greater than 0";
 }
-
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
 //#include <boost/test/unit_test.hpp>
 
 //BOOST_AUTO_TEST_SUITE(test_version)
