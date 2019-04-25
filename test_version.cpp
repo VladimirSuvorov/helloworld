a6b374c1d92f2544cc0514b237e8986028a0a8c3
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-25 16:43:29 +0300

    Update test_version.cpp

--- test_version.cpp
+++ test_version.cpp
@@ -2,8 +2,8 @@
 #include <gtest/gtest.h>
 #include "lib.h"
 
-TEST(VersionTest, VersionIsGreaterThan0) {
-   ASSERT_GT(version(), 0)<<"version is greater than 0";
+TEST(VersionTest, VersionIsGreaterThan0) {//version()
+   ASSERT_GT(0, 0)<<"version is greater than 0";
 }
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
